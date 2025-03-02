fb66ff821d67d363c225c4804a15a977b27f1ee9
Violet Nwe <narkahmynn@gmail.com>
2025-03-02 19:32:40 +1100

    Saving local changes before pulling

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -43,17 +43,48 @@ def parse():
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
 def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+	correct = recipeName.replace('-', ' ').replace('_', ' ')
+	correct = re.sub(r'[^a-zA-Z\s]', '', correct)
+	correct = ' '.join(correct.split())
+	if not correct:
+		return None
+	correct = ' '.join(word.capitalize() for word in correct.split())
+	
+	return correct
 
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
+	entry_type = data.get("type")
+	name = data.get("name")
 
+	if entry_type not in ["recipe", "ingredient"] or name in cookbook:
+		return "Invalid entry", 400
+
+	if entry_type == "ingredient":
+		cook_time = data.get("cookTime")
+		if cook_time < 0:
+			return "Invalid cookTime", 400
+		cookbook[name] = Ingredient(name, cook_time)
+
+	elif entry_type == "recipe":
+		required_items = data.get("requiredItems", [])
+		check = set()
+		items = []
+		for item in required_items:
+			if "name" not in item or "quantity" not in item:
+				return "Invalid requiredItem", 400
+			if item["name"] in check:
+				return "Duplicate requiredItem", 400
+			check.add(item["name"])
+			items.append(RequiredItem(item["name"], item["quantity"]))
+
+		cookbook[name] = Recipe(name, items)
+
+	return "", 200
 
 # [TASK 3] ====================================================================
 # Endpoint that returns a summary of a recipe that corresponds to a query name
